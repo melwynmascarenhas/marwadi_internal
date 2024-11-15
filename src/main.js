2fb5ad974b3427aa663d2d8a820d6a08b0ccf370
Melwyn <121210177+melwynmascarenhas@users.noreply.github.com>
2024-11-16 00:33:40 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -33,8 +33,6 @@ Swiper.use([
   Parallax,
 ])
 
-document.body.style.overflow = 'hidden'
-
 function enableScrolling() {
   // Enable scrolling after the delay
   document.body.style.overflowY = 'auto'
@@ -96,146 +94,30 @@ enableScrolling()
 //   )
 // //
 
-const horizontalSection = document.querySelector('.is-saboteurs')
-const wrapper = horizontalSection.querySelector('.wrapper')
-const items = wrapper.querySelectorAll('.item')
-
-const bulletWrapper = document.querySelector('.swiper-bullet-wrapper')
-bulletWrapper.style.borderRadius = '100vw'
-bulletWrapper.style.overflow = 'hidden'
-
-//////EXPLAINER SLIDER
-
-//TEXT SLIDER
-const textslider = new Swiper('.swiper_titles', {
-  slideActiveClass: 'is-active',
-  slideDuplicateActiveClass: 'is-active',
-  slidesPerView: 'auto',
-  speed: 800,
-  loop: true,
-  grabCursor: true,
-  keyboard: true,
-  mousewheel: {
-    forceToAxis: true,
-  },
-  slideToClickedSlide: true,
-  centeredSlides: true,
-  allowTouchMove: true, //click and drag to change
-  followFinger: true, //move with click and drag
-  navigation: {
-    nextEl: '.slider-next',
-    prevEl: '.slider-prev',
-  },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: bulletWrapper,
-    bulletClass: 'swiper-bullet',
-    bulletActiveClass: 'is-active',
-    bulletElement: 'button',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return (
-        '<span class="' +
-        className +
-        '">' +
-        //
-        '<div class="' +
-        'bullet-bg' +
-        '">' +
-        '</div>' +
-        //
-        '<div class="' +
-        'bullet-progress' +
-        '">' +
-        '</div>' +
-        //
-        '<div class="' +
-        'o-progress' +
-        '">' +
-        '</div>' +
-        //
-        '</span>'
-      )
+const horizontalSection = document.querySelector('.is-paths')
+if (horizontalSection) {
+  const wrapper = horizontalSection.querySelector('.wrapper')
+  const items = wrapper.querySelectorAll('.item')
+  //horizontal scrolling effect
+  let wrapperTween = gsap.to(wrapper, {
+    x: () => (wrapper.offsetWidth - window.innerWidth) * -1,
+    ease: 'none',
+    scrollTrigger: {
+      // markers: true,
+      trigger: horizontalSection,
+      pin: true,
+      start: 'top 6%',
+      end: () => `+=${items[0].offsetWidth * items.length}`,
+      scrub: 1,
+      invalidateOnRefresh: true, //recalculate the start and end points when window resize
     },
-  },
-
-  on: {
-    realIndexChange: function () {
-      // Get the real index
-      const realIndex = this.realIndex
-
-      // Loop through each bullet
-      document.querySelectorAll('.swiper-bullet').forEach((bullet, index) => {
-        const oProgress = bullet.querySelector('.o-progress')
-        // Check if the index of the bullet is less than the realIndex
-        if (realIndex == 0) {
-          if (oProgress) {
-            oProgress.style.display = 'none'
-          }
-        } else if (index < realIndex) {
-          // Find the o-progress div within the bullet and set its display to block
-          if (oProgress) {
-            oProgress.style.display = 'block'
-          }
-        }
-      })
-    },
-  },
-})
-//
-
-//horizontal scrolling effect
-let wrapperTween = gsap.to(wrapper, {
-  x: () => (wrapper.offsetWidth - window.innerWidth) * -1,
-  ease: 'none',
-  scrollTrigger: {
-    // markers: true,
-    trigger: horizontalSection,
-    pin: true,
-    start: 'top 6%',
-    end: () => `+=${items[0].offsetWidth * items.length}`,
-    scrub: 1,
-    invalidateOnRefresh: true, //recalculate the start and end points when window resize
-  },
-})
-
-const sageHeadingWrap = document.querySelector('.is-sage')
-const sageCardsWrap = document.querySelector('.sage-cards-wrap')
-
-sageCardsWrap.style.marginBottom = '-300vh'
-const cards = document.querySelectorAll('.card')
-
-gsap.set(cards[0], { rotationZ: -45 })
-gsap.set(cards[1], { rotationZ: 45 })
-gsap.set(cards[2], { rotationZ: -30 })
-
-const animatecards = gsap.timeline({
-  scrollTrigger: {
-    trigger: sageCardsWrap, // Element to trigger the animation
-    start: 'top bottom', // Start the animation when the top of the element hits the center of the viewport
-    end: 'bottom bottom', // End the animation when the bottom of the element hits the center of the viewport
-    scrub: true, // Smoothly animate elements as the user scrolls
-    pin: sageHeadingWrap,
-  },
-  ease: 'none',
-})
-animatecards
-  .to(cards[0], { y: '-100vh', rotationZ: -3 })
-  .to(cards[1], { y: '-100vh', rotationZ: 2 })
-  .to(cards[2], { y: '-100vh', rotationZ: 3 })
-//
-
-function animateNextCard() {
-  animatecards.play() // Continue playing the timeline to animate the next card
+  })
 }
 
-//TESTIMONIAL SWIPER
-const teamSlider = new Swiper('.swiper.is-testimonial', {
+//tech SWIPER
+const bulletwrap = document.querySelector('.tech-bullet-wrapper')
+const teamSlider = new Swiper('.swiper.is-tech', {
   // Parameters
-  slidesPerView: 1.5,
   spaceBetween: 16,
   breakpoints: {
     // When window width is >= 768px
@@ -255,14 +137,23 @@ const teamSlider = new Swiper('.swiper.is-testimonial', {
       slidesPerView: 3,
       spaceBetween: 24,
     },
-    1440: {
-      slidesPerView: 'auto',
-      spaceBetween: 24,
-    },
   },
-  loop: true,
+  speed: 600,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 3,
+  centeredSlides: true,
   allowTouchMove: true,
-
+  loop: true,
+  pagination: {
+    el: bulletwrap,
+    bulletClass: 'swiper-bullet',
+    bulletActiveClass: 'is-active',
+    bulletElement: 'button',
+    clickable: true,
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-btn-next',
@@ -270,91 +161,215 @@ const teamSlider = new Swiper('.swiper.is-testimonial', {
   },
 })
 
+//story SWIPER
+const storybulletwrap = document.querySelector('.story-bullet-wrapper')
+const storySlider = new Swiper('.swiper.is-story', {
+  // Parameters
+  spaceBetween: 16,
+  breakpoints: {
+    // When window width is >= 768px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 16,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+  speed: 600,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 3,
+  centeredSlides: true,
+  allowTouchMove: true,
+  loop: true,
+  pagination: {
+    el: storybulletwrap,
+    bulletClass: 'swiper-bullet',
+    bulletActiveClass: 'is-active',
+    bulletElement: 'button',
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+})
+
+//close form and open form
+const apply = document.querySelector('[apply]')
+if (apply) {
+  apply.addEventListener('click', function () {
+    document.querySelector('.popup').style.display = 'flex'
+  })
+
+  document.querySelector('.close-form').addEventListener('click', function () {
+    document.querySelector('.popup').style.display = 'none'
+  })
+}
+
 //FLIP MENU
-gsap.registerPlugin(Flip)
-let hamburgerEl = document.querySelector('.nav_hamburger_wrap')
-let navLineEl = document.querySelectorAll('.nav_hamburger_line')
-let flipItemEl = document.querySelector('.nav_hamburger_base')
-let menuWrapEl = document.querySelector('.mob_menu_wrap')
-let menuBaseEl = document.querySelector('.menu_base')
-let menuContainEl = document.querySelector('.mob_menu_contain')
+// gsap.registerPlugin(Flip)
+// let hamburgerEl = document.querySelector('.nav_hamburger_wrap')
+// let navLineEl = document.querySelectorAll('.nav_hamburger_line')
+// let flipItemEl = document.querySelector('.nav_hamburger_base')
+// let menuWrapEl = document.querySelector('.mob_menu_wrap')
+// let menuBaseEl = document.querySelector('.menu_base')
+// let menuContainEl = document.querySelector('.mob_menu_contain')
 
-let flipDuration = 0.6
+// let flipDuration = 0.6
 
-function flip(forwards) {
-  let state = Flip.getState(flipItemEl)
-  if (forwards) {
-    menuContainEl.appendChild(flipItemEl)
-  } else {
-    hamburgerEl.appendChild(flipItemEl)
-  }
-  Flip.from(state, { duration: flipDuration })
-}
+// function flip(forwards) {
+//   let state = Flip.getState(flipItemEl)
+//   if (forwards) {
+//     menuContainEl.appendChild(flipItemEl)
+//   } else {
+//     hamburgerEl.appendChild(flipItemEl)
+//   }
+//   Flip.from(state, { duration: flipDuration })
+// }
 
-let tl = gsap.timeline({ paused: true })
-tl.set(menuWrapEl, { display: 'flex' })
-//from is used because we want to move the base first
-tl.from(menuBaseEl, {
-  opacity: 0,
-  duration: flipDuration,
-  ease: 'none',
-  //conditional...only runs when the timeline starts
-  onStart: function () {
-    flip(true)
-  },
+// let tl = gsap.timeline({ paused: true })
+// tl.set(menuWrapEl, { display: 'flex' })
+// //from is used because we want to move the base first
+// tl.from(menuBaseEl, {
+//   opacity: 0,
+//   duration: flipDuration,
+//   ease: 'none',
+//   //conditional...only runs when the timeline starts
+//   onStart: function () {
+//     flip(true)
+//   },
+// })
+// tl.to(navLineEl[0], { y: 4, rotate: 45, duration: flipDuration }, '<')
+// tl.to(navLineEl[1], { y: -4, rotate: -45, duration: flipDuration }, '<')
+
+// const menuLinks = gsap.utils.toArray('.menu_link')
+// tl.from(menuLinks, {
+//   opacity: 0,
+//   yPercent: 50,
+//   duration: 0.2,
+//   stagger: { amount: 0.2 },
+//   //conditional...only runs when the tween finishes the reverse to the start point
+//   //here moving the base after the links disappear completely
+//   onReverseComplete: function () {
+//     flip(false)
+//   },
+// })
+
+// function openMenu(open) {
+//   //check if the animation is playing to stop intteruption
+//   if (!tl.isActive()) {
+//     if (open) {
+//       tl.play()
+//       hamburgerEl.classList.add('nav-open')
+//       document.body.style.overflow = 'hidden'
+//     } else {
+//       //play close animation because because open menu was set to false
+//       tl.reverse()
+//       hamburgerEl.classList.remove('nav-open')
+//       document.body.style.overflow = ''
+//     }
+//   }
+// }
+
+// //callback to perform menu open or close
+// hamburgerEl.addEventListener('click', function () {
+//   //checking if the menu is open or closed
+//   if (hamburgerEl.classList.contains('nav-open')) {
+//     //then set openmenu to false
+//     openMenu(false)
+//   } else {
+//     openMenu(true)
+//   }
+// })
+
+// menuBaseEl.addEventListener('mouseenter', function () {
+//   openMenu(false)
+// })
+// menuBaseEl.addEventListener('click', function () {
+//   openMenu(false)
+// })
+
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape') {
+//     openMenu(false)
+//   }
+// })
+
+///stick Main nav
+// Pin the mainNav element until the entire page has been scrolled
+// gsap.to('.section-padding.is-nav', {
+//   scrollTrigger: {
+//     trigger: '.section-padding.is-nav',
+//     start: 'top top', // when the top of the nav matches the top of the viewport
+//     end: () =>
+//       document.documentElement.scrollHeight -
+//       document.documentElement.clientHeight,
+//     pin: true, // pin it in place
+//     pinSpacing: false, // removes the extra space added by pinning
+//   },
+// })
+
+const pinItems = document.querySelectorAll('[pin]')
+pinItems.forEach((item) => {
+  gsap.to(item, {
+    scrollTrigger: {
+      trigger: item,
+      start: 'top top', // when the top of the nav matches the top of the viewport
+      end: () =>
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight,
+      pin: true, // pin it in place
+      pinSpacing: false, // removes the extra space added by pinning
+    },
+  })
 })
-tl.to(navLineEl[0], { y: 4, rotate: 45, duration: flipDuration }, '<')
-tl.to(navLineEl[1], { y: -4, rotate: -45, duration: flipDuration }, '<')
 
-const menuLinks = gsap.utils.toArray('.menu_link')
-tl.from(menuLinks, {
-  opacity: 0,
-  yPercent: 50,
-  duration: 0.2,
-  stagger: { amount: 0.2 },
-  //conditional...only runs when the tween finishes the reverse to the start point
-  //here moving the base after the links disappear completely
-  onReverseComplete: function () {
-    flip(false)
-  },
-})
+const effectSections = document.querySelectorAll('[effect-section]')
 
-function openMenu(open) {
-  //check if the animation is playing to stop intteruption
-  if (!tl.isActive()) {
-    if (open) {
-      tl.play()
-      hamburgerEl.classList.add('nav-open')
-      document.body.style.overflow = 'hidden'
-    } else {
-      //play close animation because because open menu was set to false
-      tl.reverse()
-      hamburgerEl.classList.remove('nav-open')
-      document.body.style.overflow = ''
-    }
-  }
-}
+effectSections.forEach((effectSection) => {
+  const textBlocks = effectSection.querySelectorAll('[text-block]')
+  const imgBlocks = effectSection.querySelectorAll('[img-block]')
 
-//callback to perform menu open or close
-hamburgerEl.addEventListener('click', function () {
-  //checking if the menu is open or closed
-  if (hamburgerEl.classList.contains('nav-open')) {
-    //then set openmenu to false
-    openMenu(false)
-  } else {
-    openMenu(true)
-  }
-})
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: effectSection,
+      start: 'top bottom',
+      end: 'top center',
+      toggleActions: 'none play none reset',
+    },
+  })
 
-menuBaseEl.addEventListener('mouseenter', function () {
-  openMenu(false)
-})
-menuBaseEl.addEventListener('click', function () {
-  openMenu(false)
-})
+  tl.from(textBlocks, {
+    y: '10%', // animate from below
+    opacity: 0, // animate from opacity 0
+    duration: 3, // specify the duration (1 second in this case)
+    ease: 'quart', // use 'quart' for quart easing
+    stagger: 0.2, // stagger the animations with a delay of 0.2 seconds
+  })
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape') {
-    openMenu(false)
-  }
+  tl.from(
+    imgBlocks,
+    {
+      y: '10%', // animate from below
+      opacity: 0, // animate from opacity 0
+      duration: 2, // specify the duration (1 second in this case)
+      ease: 'quart', // use 'quart' for quart easing
+      stagger: 0.2, // stagger the animations with a delay of 0.2 seconds
+    },
+    '<'
+  )
 })
